92c7617b47c184aa359dc15883080105f5352601
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-09 18:46:33 -0400

    [ADD] created .env and implement it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
+  providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = this.configService.get('BCKND_PORT');
+  }
+}
