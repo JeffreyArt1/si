945bdb13434c897b60f9c2f659b5497393d58297
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-09 18:48:57 -0400

    Si

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './modules/auth/auth.controller';
+import { ListingController } from './modules/listing/listing.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
+  controllers: [AppController, ListingController, AuthController],
   providers: [AppService, ConfigService],
 })
 export class AppModule {
