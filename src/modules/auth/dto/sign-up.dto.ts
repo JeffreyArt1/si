401ba8dd3f77c07b3a1648b656d7beefa9ab671d
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-10 18:47:25 -0400

    [ADD] dto

---
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
