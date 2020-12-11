488c2d9040ff7e838b9bbfa23845822c8b980960
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-11 01:00:59 -0400

    [IMP] implemente guards

--- src/modules/listing/listing.controller.ts
+++ src/modules/listing/listing.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards';
 
 @Controller('listing')
 export class ListingController {
   @Get()
+  @UseGuards(JwtAuthGuard)
   getListing(): string {
     return 'Compay';
   }
