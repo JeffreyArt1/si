74085a9e85df15fa57f8d850361285b22a240900
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-11 01:01:10 -0400

    [IMP] implemented cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(helmet());
   app.use(compression());
+  app.use(cookieParser());
 
   await app.listen(AppModule.port);
 }
