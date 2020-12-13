6b1e2aad33cc192a9acb427bb4282d33d8e8d759
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-13 18:19:09 -0400

    [REF] make config module global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,12 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ListingModule } from './modules/listing/listing.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule, ListingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    ListingModule,
+  ],
 })
 export class AppModule {
   static port: number;
