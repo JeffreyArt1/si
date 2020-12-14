eef89ccd32860a10b5709169f2c16fb05861e4ed
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-14 04:19:11 -0400

    [REF] created index for interfaces - also renamed file to fit convention

---
+++ src/modules/auth/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { JwtPayload } from './jwt-payload.interface';
+export { RequestWithtUser } from './request-with-user.interface';
--- src/modules/auth/interfaces/request-with-user.interface.ts
+++ src/modules/auth/interfaces/request-with-user.interface.ts
@@ -1,4 +1,4 @@
-import { User } from '../../users/entities/';
+import { User } from '../../users/entities';
 
 export interface RequestWithtUser extends Request {
   user: User;
