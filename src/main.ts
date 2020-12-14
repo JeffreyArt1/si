32cb1e738ad4a84ae3bf3e7c248ef05cb1c185d8
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-14 08:20:28 -0400

    [REF] added validationPipe

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.useGlobalFilters(
     new HttpExceptionFilter(),
     new DatabaseExceptionFilter(),
