aa081f67f007e1258904ed24e258634ba91a8fcb
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-14 02:31:43 -0400

    [REM] deprecated file

--- src/utils/functions/http-exception-thrower.ts
+++
@@ -1,9 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { HttpExceptionMessages } from '../enums/http-exception-messages.enum';
-
-export function HttpExceptionThrower(
-  message: HttpExceptionMessages,
-  status: HttpStatus,
-) {
-  throw new HttpException(message, status);
-}
