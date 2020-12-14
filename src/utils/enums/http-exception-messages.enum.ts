2b6bc4a87bbcff8d3a69fb9b7c54a090796321f0
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-14 07:46:58 -0400

    [ADD] more exceptionmesages

--- src/utils/enums/http-exception-messages.enum.ts
+++ src/utils/enums/http-exception-messages.enum.ts
@@ -1,7 +1,9 @@
 export enum HttpExceptionMessages {
   EMAIL_NOT_EXISTS = "Oops! this email doesn't seem to exist in our DB",
+  EMAIL_INVALID = "Oops! this email doesn't seem to be correck. Check it and try again.",
   EMAIL_EXISTS = 'Oops! this email exists already!',
   EMAIL_NOT_VERIFIED = "Oops! this account isn't verified yet!",
   INTERNAL_ERROR = 'Oops! Something happened... something happened',
-  BAD_PASSWD = 'Oops! wrong password',
+  BAD_PASSWD = 'Oop""s! wrong password',
+  EMPTY_RESPONSE = "There's nothing to see here, Compai. Go make some requests and check again.",
 }
