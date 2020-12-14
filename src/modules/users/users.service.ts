ac2e838e60d4bede7206c928f8716dcc46fe3e4b
Jeffrey Mesa <JeffreyArt@hotmail.com>
2020-12-14 06:02:32 -0400

    [FIX] use find method properly for the sent value

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -28,7 +28,7 @@ export class UsersService {
   }
 
   async userExists(email: string): Promise<User> {
-    const user = await this.userRepository.findOne(email);
+    const user = await this.userRepository.findOne({ where: [{ email }] });
     return user ? user : null;
   }
 }
